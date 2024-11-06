c1d6a5ff8f789014f65aaf62d1582fe98e4df276
jakhongirav <jakhongir.av@gmail.com>
2024-11-06 16:57:23 +0100

    initial commit

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -8,14 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        EmptyView()
     }
 }
 
