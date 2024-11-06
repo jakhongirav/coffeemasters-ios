6f8db6ea5267072bbfcf68e0748911695fef1c18
jakhongirav <jakhongir.av@gmail.com>
2024-11-06 16:56:07 +0100

    initial commit

---
+++ CoffeeMasters/Offer.swift
@@ -0,0 +1,18 @@
+//
+//  Offer.swift
+//  CoffeeMasters
+//
+//  Created by Jahongir Abdujalilov on 06/11/24.
+//
+
+import SwiftUI
+
+struct Offer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Offer()
+}
