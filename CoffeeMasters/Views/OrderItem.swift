9cf4037611c4cabcf41fb8305835a024d2b227c9
jakhongirav <jakhongir.av@gmail.com>
2024-11-12 16:02:30 +0100

    Products

---
+++ CoffeeMasters/Views/OrderItem.swift
@@ -0,0 +1,18 @@
+//
+//  OrderItem.swift
+//  CoffeeMasters
+//
+//  Created by Jahongir Abdujalilov on 12/11/24.
+//
+
+import SwiftUI
+
+struct OrderItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderItem()
+}
